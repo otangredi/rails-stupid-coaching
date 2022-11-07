73c5af0474815a90a4371124563b14e72a5cbf4c
Olivia Tangredi oliviatangredixo@gmail.com <oliviatangredixo@gmail.com>
2022-11-07 12:28:18 -0500

    question plus answer is displayed

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+    @question
+  end
+
+  def answer
+    @question = params[:question]
+    @response = ''
+
+    if @question == 'I am going to work'
+      @response = 'Great!'
+    elsif @question.end_with?('?')
+      @response = 'Silly question, get dressed and go to work!'
+    else
+      @response = "I don't care, get dressed and go to work!"
+    end
+  end
+end
